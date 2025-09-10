635fed645314c73ceafeeabbf80a93bc65eb0699
Rafel Amengual Tomás <ramengualtomas@cifpfbmoll.eu>
2025-09-10 15:45:36 +0200

    Update 0.1v

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Info } from "./components/info/info";
+import { Timeline } from './components/timeline/timeline';
+import { Footer } from './components/footer/footer'
+import { Proyectos } from './components/proyectos/proyectos'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Info, Timeline, Proyectos, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Portfolio');
 }
