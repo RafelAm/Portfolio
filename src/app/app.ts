553e45714b397e3c1ad7b1644bf3651cc1905ab2
Rafel Amengual Tomás <ramengualtomas@cifpfbmoll.eu>
2025-09-18 16:47:28 +0200

    Update 0.8v
    Añadido componente de contacto y modificados varios tamaños de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { Timeline } from './components/timeline/timeline';
 import { Footer } from './components/footer/footer'
 import { Proyectos } from './components/proyectos/proyectos'
 import { Skills } from './components/skills/skills';
+import { Contact } from './components/contact/contact'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Info, Timeline, Proyectos, Footer, Skills],
+  imports: [RouterOutlet, Header, Info, Timeline, Proyectos, Footer, Skills , Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
